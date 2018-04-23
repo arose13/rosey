4ae5f6fee72a45729179f58c03153f0149416ab4
arose13 <anthony_rose12@hotmail.com>
2018-04-23 11:08:12 -0400

    roc updated

--- rosey/graphing.py
+++ rosey/graphing.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as graph
 
 
-def plot_roc_curve(prediction_probability, true, label='', show_graph=False):
+def plot_roc_curve(prediction_probability, true, label='', plot_curve_only=False, show_graph=False):
     from sklearn.metrics import roc_curve, roc_auc_score
     label = label if label == '' else label + ' '
 
@@ -9,7 +9,8 @@ def plot_roc_curve(prediction_probability, true, label='', show_graph=False):
     auc = roc_auc_score(true, prediction_probability)
 
     graph.plot(fpr, tpr, label='{}AUC = {}'.format(label, auc))
-    graph.plot([0, 1], [0, 1], label='Guessing')
+    if not plot_curve_only:
+        graph.plot([0, 1], [0, 1], linestyle='--', color='k', label='Guessing')
     graph.xlim([0, 1])
     graph.ylim([0, 1])
     graph.legend(loc=0)
