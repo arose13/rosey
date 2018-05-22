af0626d2f62249007cdf436353d1c53a21ae82ef
arose13 <anthony_rose12@hotmail.com>
2018-05-22 11:18:45 -0400

    removed optional dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 from os import path
 
 # noinspection SpellCheckingInspection
-__version__ = '1.20180515'  # YYYYMMDD
+__version__ = '1.20180522'  # YYYYMMDD
 
 here = path.abspath(path.dirname(__file__))
 
