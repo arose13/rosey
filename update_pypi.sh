da57a1a5e8dfc35b9609ea8843c9086c697877da
arose13 <anthony_rose12@hotmail.com>
2018-05-28 16:40:29 -0400

    automatically archive old distributions when pushing new ones

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Requires package `twine`
 # Requires `.pypirc` file to be in the home directory. eg `~/.pypirc`
+mv dist/*tar.gz old_dist/
 
 python setup.py sdist
 twine upload dist/*
