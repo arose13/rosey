235d7bd0854d2166662d8edc20e4db58e814b2ff
arose13 <anthony_rose12@hotmail.com>
2019-03-19 12:28:01 -0400

    version change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 from os import path
 
 # noinspection SpellCheckingInspection
-__version__ = '1.20181020'  # YYYYMMDD
+__version__ = '1.20190319'  # YYYYMMDD
 
 here = path.abspath(path.dirname(__file__))
 
